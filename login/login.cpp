9a346504ac27756c811e926927846fdfd6ecc942
Richesh Gupta <rgrichesh45@gmail.com>
2019-05-01 14:05:27 +0530

    changed architecture of the project

--- login/login.cpp
+++ login/login.cpp
@@ -1,79 +1,78 @@
 #include<iostream>
 #include<fstream>
+#include<stdlib.h>
 using namespace std;
-int login_flag;
-class User
+int login_status  = 0;
+class credentials
 {
-string user,pass;
-
+	string username;
+	string password;
 public:
-void get_input()
-{
-    cout<<"enter user-id!\n";
-    cin>>user;
-    cout<<"Enter password\n";
-    cin>>pass;
-}
-string retuser()
-{
-    return user;
-}
-string retpass()
-{
-    return pass;
-}
-void write_cred()
-{
-    ofstream f1;
-    f1.open("login/users_db.dat",ios::binary|ios::app);
-    (*this).get_input();
-    f1.write((char*)this,sizeof(this));
-    f1.close();
-}
-int authenticate();
-void print_data();
-void output();
+	void input_credentials();
+	void write_credentials();
+	void credentials_register();
+	void credentials_login();
+	void credentials_logout();
 };
-int User::authenticate()
-{
-    ifstream f1;
-    string temp_u,temp_p;
-    cout<<"Enter user id\n";
-    cin>>temp_u;
-    cout<<"Enter password\n";
-    cin>>temp_p;
-    f1.open("login/login.cpp",ios::binary);
-    while(f1.read((char*)this,sizeof(this)))
-    {
-        if(this->retuser() == temp_u)
-        {
-            if(this->retpass() == temp_p)
-            {
-                login_flag=1;
-            }
-            else
-            {
-               login_flag = 0;
-            }
-        }
-    }
-    f1.close();
-return login_flag;
-}
+void credentials::input_credentials()
+{	
+		fstream f1;
+		f1.open("login/users_db.dat",ios::binary);
+		cout<<"\n Enter Username\n";
+		cin>>username;
+		string temp_u = username;
+		while(f1.read((char*)this,sizeof(this)))
+		{
+			if(this->username == temp_u)
+				cout<<"Username already exists\n";
+		}
+		cout<<"\n Enter password\n";
+		cin>>password;
+	}
 
-void User::output()
+void credentials::write_credentials()
 {
-    cout<<"\nUser id - "<<user;
-    cout<<"\npass \n -"<<pass;
+	fstream f1;
+	f1.open("login/users_db.dat",ios::app|ios::binary);
+	this->input_credentials();
+	f1.write((char*)this,sizeof(this));
+	cout<<"\nRegistered Successfully!\n";
+	f1.close();
 }
-void User::print_data()
+void credentials::credentials_register()
 {
-    ifstream f1;
-    User b;
-    f1.open("login/login.cpp",ios::binary);
-    while(f1.read((char*)&b,sizeof(b)))
-    {
-        b.output();
-    }
-    f1.close();
+	this->write_credentials();
+}
+void credentials::credentials_login()
+{	string temp_u,temp_p;
+	cout<<"\n\t\t -- login --\n\n";
+	cout<<"\nEnter username : ";cin>>temp_u;
+	cout<<"\nEnter password : ";cin>>temp_p;
+
+	fstream f1;
+	f1.open("login/users_db",ios::binary);
+	while(f1.read((char*)this,sizeof(this)))
+	{
+		if(this->username == temp_u)
+		{
+			if(this->password == temp_p)
+			{
+				login_status = 1;
+				break;
+			}
+		}
+	}
+	cout<<"\nLogin was Successfull\n";
+
+}
+void credentials::logout()
+{
+	if(login_status)
+	{
+		login_status = 0;
+	}
+	else
+	{
+		cout<<"Not logged in!";
+	}
 }
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include"login/login.cpp"
+
+int main(void)
+{
+	credentials c;
+	c.credentials_register();
+	c.credentials_login();
+	return 0;
+}
