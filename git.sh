b8f599d89ee955f3aea1fe80fbce891b966ef6e9
Richesh Gupta <rgrichesh45@gmail.com>
2019-05-03 23:16:54 +0530

    $comment

--- git.sh
+++ git.sh
@@ -1,3 +1,11 @@
+echo "we are in :"
+pwd
 echo "Enter commit comment"
 read comment
-git add . && git commit -m '$comment' && git push
+git add . && git commit -m '$comment'
+echo "do you want to push now [y/n]"
+read var
+if [[ var ='y' ]]; then
+	git push
+fi
+	exit
