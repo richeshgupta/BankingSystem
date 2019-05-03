de9cf9f410f00a8876b94eb2d9692c1a77b96b73
Richesh Gupta <rgrichesh45@gmail.com>
2019-05-03 23:20:59 +0530

    bash test push

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@ echo "we are in :"
 pwd
 echo "Enter commit comment"
 read comment
-git add . && git commit -m '$comment'
+git add . && git commit -m "$comment"
 echo "do you want to push now [y/n]"
 read var
 if [[ $var -eq 'y' ]]; then
